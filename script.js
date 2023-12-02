d693dffead76ea3acc99a6a02cf55786dd1d7ec2
ashiqmuneeb <132690535+ashiqmuneeb@users.noreply.github.com>
2023-12-02 11:38:25 +0530

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
     $('form[name="signupForm"]').submit(function (event) {
-        // Prevent the form from submitting
+      
         event.preventDefault();
 
-        // Get the input values
+        
         var fname = $('#text1').val();
         var lname = $('#text2').val();
         var email = $('#number').val();
         var password = $('#password').val();
 
-        // Validate the inputs (add your own validation logic here)
+       
         if (fname === '') {
             alert('First name is required.');
             return;
@@ -27,7 +27,35 @@ $(document).ready(function () {
             return;
         }
 
-        // If all validation passes, you can submit the form to the server
+        
         $('form[name="signupForm"]').unbind('submit').submit();
     });
 });
+
+
+$(document).ready(function() {
+    
+    var $daySelect = $('select[name="day"]');
+    for (var i = 1; i <= 31; i++) {
+        $daySelect.append('<option value="' + i + '">' + i + '</option>');
+    }
+
+    
+    var months = [
+        'January', 'February', 'March', 'April',
+        'May', 'June', 'July', 'August',
+        'September', 'October', 'November', 'December'
+    ];
+    var $monthSelect = $('select[name="month"]');
+    for (var j = 0; j < months.length; j++) {
+        $monthSelect.append('<option value="' + (j + 1) + '">' + months[j] + '</option>');
+    }
+
+    
+    var currentYear = new Date().getFullYear();
+    var $yearSelect = $('select[name="year"]');
+    for (var k = currentYear; k >= 1900; k--) {
+        $yearSelect.append('<option value="' + k + '">' + k + '</option>');
+    }
+});
+
